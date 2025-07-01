94a72fc725219f5345342519b798848956bdbe82
Amogh <amoghsrivastava7080@gmail.com>
2025-07-01 13:17:18 +0530

    updated

--- missing_number.py
+++ missing_number.py
@@ -11,22 +11,3 @@ def missing_n(arr):
 print(missing_n([1,3,0,5]))
 
 
-# BRUTE FORCE
-
-# def miss(arr):
-#     d={}
-
-#     for i in range(0,len(arr)):
-#         if arr[i] in d:
-#             d[arr[i]]+=1
-
-#         else:
-#             d.update({arr[i] : 1})
-
-    
-#     for k,v in d.items():
-#         if v==1:
-#             return(k)
-        
-
-# print(miss([12,12,2,2,3]))
--- single_number.py
+++ single_number.py
@@ -1,13 +1,13 @@
+# The XOR operation returns false if any of operands are same ( f ,f = f or t ,t = f)
+
+
+
 def single_num(arr):
-    
+    xor=0
     for i in arr:
-        ct=0
-        for j in arr:
-            if i==j:
-                ct+=1
-            print(ct)  
-            
-        
-    
-single_num([1,1,2,2,0])
-            
+        xor^=i
+
+    return xor
+
+
+print(single_num([1,1,2,2,3,4,4]))
